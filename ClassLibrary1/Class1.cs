6e8454db19ad4fb3cc49086d16c91b199117da91
SaZyGH <103922719+SaZyGH@users.noreply.github.com>
2022-04-26 09:17:22 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    public class Application
+    {
+        List<TaskList> taskLists;
+        Application application;
+
+        public static void GetApplication() 
+        {
+
+        }
+
+    }
+    public class TaskList
+    {
+        String name;
+        List<Task> tasks;
+    }
+    public class Task
+    {
+        String Title;
+        DateTime Due;
+        Boolean Done;
+
+    }
+}
